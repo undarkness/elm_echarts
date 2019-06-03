8464a9d61361b4a113635d12594ee4d4bce6c243
undarkness <407645831@qq.com>
2019-06-03 19:26:10 +0800

    123

--- src/main.js
+++ src/main.js
@@ -3,25 +3,25 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
-import Echarts from 'echarts'
-// import myCharts from './utils/myCharts.js'
+import echarts from 'echarts'
+import myCharts from './utils/myCharts.js'
 import App from './App.vue'
 
 // import store from './vuex/store'
-import routers from './router/routers'
+import routes from './router/routes'
 // import Mock from './mock'
 
 
 Vue.config.productionTip = false;
-Vue.prototype.echarts = Echarts;
+// Vue.prototype.$charts = echarts;
 
 Vue.use(ElementUI);
 Vue.use(VueRouter);
 Vue.use(Vuex);
-// Vue.use(myCharts);/
+Vue.use(myCharts);
 
 const router = new VueRouter({
-  routers
+  routes
 })
 new Vue({
   el: '#app',
